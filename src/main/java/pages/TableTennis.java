45cf495c59a622da09d77f6a0efb85c205599b7e
georgi kotsev <georgi.kotsev@dormakaba.com>
2025-05-26 09:59:45 +0300

    delete unused code

--- src/main/java/pages/TableTennis.java
+++ src/main/java/pages/TableTennis.java
@@ -73,51 +73,6 @@ public class TableTennis extends BaseTools {
         assertSportPageLoaded("Table-Tennis");
     }
 
-
-
-
-
-
-
-
-
-
-//    public String getTotalNmber(){
-//        waitForElementVisibility(result);
-//
-//        String resulttext = result.getText();
-//        String regex = "общо\\s(\\d+)";
-//
-//        java.util.regex.Pattern pattern = java.util.regex.Pattern.compile(regex);
-//        java.util.regex.Matcher matcher = pattern.matcher(resulttext);
-//
-//        String resultNumber = null;
-//
-//        // Ensure there is a match before calling group()
-//        if (matcher.find()) {
-//            resultNumber = matcher.group(1);  // Get the number after "общо"
-//            LOGGER.info("Total number of searched cars is: " + resultNumber);
-//        } else {
-//            LOGGER.warn("No number found after 'общо' in the result text.");
-//        }
-//
-//        return resulttext;
-//
-//    }
-//
-//    public int countVipAdons(){
-//        int size = promotedVipAds.size();
-//        LOGGER.info("Total number of searched VIP adons is: " + size);
-//        return size;
-//    }
-//
-//    public int countTopAdons(){
-//        int size = promotedTopAds.size();
-//        LOGGER.info("Total number of searched TOP adons is: " + size);
-//        return size;
-//    }
-
-
 }
 
 
